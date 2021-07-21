12b26df13802084d3d61b2a6dd25f5f534ab2480
Ocky Harliansyah <ocky.harli@gmail.com>
2021-07-21 14:26:10 +0700

    admin template

---
+++ public/cork1/assets/js/authentication/form-2.js
@@ -0,0 +1,29 @@
+var togglePassword = document.getElementById("toggle-password");
+var togglePasswordConfirmation = document.getElementById("toggle-password-confirmation");
+var formContent = document.getElementsByClassName('form-content')[0]; 
+var getFormContentHeight = formContent.clientHeight;
+
+var formImage = document.getElementsByClassName('form-image')[0];
+if (formImage) {
+	var setFormImageHeight = formImage.style.height = getFormContentHeight + 'px';
+}
+if (togglePassword) {
+	togglePassword.addEventListener('click', function() {
+	  var x = document.getElementById("password");
+	  if (x.type === "password") {
+	    x.type = "text";
+	  } else {
+	    x.type = "password";
+	  }
+	});
+}
+if (togglePasswordConfirmation) {
+	togglePasswordConfirmation.addEventListener('click', function() {
+	  var x = document.getElementById("password_confirmation");
+	  if (x.type === "password") {
+	    x.type = "text";
+	  } else {
+	    x.type = "password";
+	  }
+	});
+}
